f24d20d7b0ecd00ecfbc94f6a1ae257f0efe12a2
AithusaTm <117110936+AithusaTm@users.noreply.github.com>
2023-06-12 13:37:12 -0600

    Update grafica6.py

--- grafica6.py
+++ grafica6.py
@@ -1,16 +1,16 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-xpoints = np.linspace(0,2*np.pi,100)
+xpoints = np.linspace(0,2,200)
 def f(x):
     return (xpoints*np.exp(-3*xpoints))
 plt.plot(xpoints,f(xpoints),'o:c',ms=10)
-plt.title('$f(x)=xe^{-3x}$\n$g(x)=1-3xe^{-3x}$')
+plt.title('$f(x)= xe^{-3x}$\n$g(x)=e^{-3x}(1-3x)$')
 plt.ylabel('Eje y')
 plt.xlabel('Eje x')
 
 def y(x):
-    return (1-(3*xpoints*np.exp(-3*xpoints)))
+    return np.exp(-3*xpoints)*(1-3*xpoints)
 plt.plot(xpoints,y(xpoints),'o:m', ms=10)
 plt.legend(['f(x)','g(x)'])
 plt.show()
